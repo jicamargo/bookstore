7fe1c768c3cff3ffe3017bdc92d659c9f36462e6
jic <ji_camargo@yahoo.com>
2023-06-20 11:26:18 -0500

    refactor categoriesSlice.js, add checkStatus reducer

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
 function Categories() {
-  const categoriesState = useSelector((state) => state.categories);
-
-  if (categoriesState === 'Under construction') {
-    return (
-      <div>
-        <h1>Categories Page</h1>
-        <p>Under construction</p>
-      </div>
-    );
-  }
   return (
     <div>
       <h1>Categories Page</h1>
-      <p>New page</p>
+      <p>Under construction</p>
     </div>
   );
 }
--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,13 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = 'Under fconstruction'; // Set the initial state as "Under construction"
+const initialState = []; // Set the initial state as an empty array
 const categoriesSlice = createSlice({
   name: 'categories',
   initialState,
   reducers: {
-    // reducers specific to categories if needed
+    checkStatus: (state, action) => {
+      if (action.payload === 'Under Construction') {
+        return 'Under Construction';
+      }
+      return state;
+    },
+    // Add other reducers if needed
   },
 });
 
-export const { actions } = categoriesSlice;
+export const { checkStatus } = categoriesSlice.actions;
 export default categoriesSlice.reducer;
